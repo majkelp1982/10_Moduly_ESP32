4e1ab5a3b30b8b34b1a9e26edec3236f9116ebb7
majkelp1982 <s.pomazanka@gmail.com>
2021-05-03 10:23:40 +0200

    impl

--- Module_LIBS/Basic/Basic.h
+++ Module_LIBS/Basic/Basic.h
@@ -4,7 +4,7 @@
 #include <esp_task_wdt.h>
 
 #define VERSION				"2021.04.25"
-#define BETA_VERSION		"_speed_optimization2"
+#define BETA_VERSION		"_osw_zew_first_release2"
 
 #define FIRMWARE_VERSION	VERSION BETA_VERSION
 
--- modul_osw_zew/modul_osw_zew.ino
+++ modul_osw_zew/modul_osw_zew.ino
@@ -28,6 +28,6 @@ void loop()
 	ota.client();
 	basic.run();
 	communication.run();
-	status.printStatus(10);
+	status.printStatus(1);
 	module();
 }
